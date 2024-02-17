6b84bd7f632ee023dbbfc9913864c4f44cbd0a8a
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 17:48:36 +0530

    added: (all variabels with comments)

--- Basics1/vars.java
+++ Basics1/vars.java
@@ -2,9 +2,18 @@ package Basics1;
 
 public class vars {
     public static void main(String[] args) {
-        int num = 3;
+        int num = 69;
         String str = "Ved";
-        System.out.println(num);
-        System.out.println(str);
+        char ch = 'V';
+        float fl = 6.9f;
+        double db = 6.9696;
+        boolean bool = true;
+        System.out.println(num); // this will print the value stored in the variables num
+        System.out.println(str); // this will print the value stored in the variables str 
+        System.out.println(ch); // this will print the value stored in the variables ch
+        System.out.println(fl); //  here remember to add f after t5he value so that it will print the float value (no need to convert in to double ) this will print the value stored in the variables fl
+        System.out.println(db); // this will print the value stored in the variables db
+        System.out.println(bool); // this will print the value stored in the variables bool 
+
     }
 }
