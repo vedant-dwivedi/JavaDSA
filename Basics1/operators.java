7d8323020459e29e3cedc22d542d5c9fe896e70d
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 01:54:41 +0530

    added: (all the operators functions)

--- Basics1/operators.java
+++ Basics1/operators.java
@@ -2,8 +2,16 @@ package Basics1;
 
 public class operators {
     public static void main(String[] args) {
-        int num = 69;
-
-        System.out.println(num);
+        int num1 = 11;
+        int num2 = 3;
+        System.out.println("sum: " + num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println("multiply: " + num1*num2);
+        System.out.println("division: " + num1/num2); // tells the division
+        System.out.println("modulus: " + num1%num2); // tells the remainder
+        System.out.println("preinc: " + ++num1); 
+        System.out.println("postinc: " + num2++);
+        System.out.println("postdec: " + num2--);
+        System.out.println("predec: " + --num2);
     }
 }
