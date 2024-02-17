f6871b6024c8580c8bcd0f9dc5dac865164cc93a
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 18:35:45 +0530

    added: (printing multiple variables)

--- Basics1/multiVar.java
+++ Basics1/multiVar.java
@@ -2,6 +2,12 @@ package Basics1;
 
 public class multiVar { 
    public static void main(String[] args) {
-        System.out.println(); 
+      int x = 69;
+      int y = 96;
+      int z = 94;
+
+      int a = 2, b = 3, c = 4;
+        System.out.println(x+y+z); 
+        System.out.println(a+b+c); 
    }
 }
