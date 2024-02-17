ab837ebfa5b3221a8a7ece6df3eb5f1e68909ae3
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 01:36:19 +0530

    formatted

---
+++ Basics1/typeCasting.java
@@ -0,0 +1,10 @@
+package Basics1;
+
+public class typeCasting { 
+   public static void main(String[] args) {
+    int num = 69;
+    double numd = num;
+        System.out.println(num); 
+        System.out.println(numd); 
+   }
+}
---
+++ Basics1/typeCasting2.java
@@ -0,0 +1,10 @@
+package Basics1;
+
+public class typeCasting2 { 
+   public static void main(String[] args) {
+    double numd = 6.9;
+    int num = (int) numd;
+        System.out.println(numd); 
+        System.out.println(num); 
+   }
+}
