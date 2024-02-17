e40209586df37afa0ced3862d59aeafd40c82175
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 17:39:51 +0530

    variables

---
+++ Basics1/vars.java
@@ -0,0 +1,10 @@
+package Basics1;
+
+public class vars {
+    public static void main(String[] args) {
+        int num = 3;
+        String str = "Ved";
+        System.out.println(num);
+        System.out.println(str);
+    }
+}
