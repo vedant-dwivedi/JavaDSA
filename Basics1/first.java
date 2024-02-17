26bf8541448135acc14e8d580e3b27cfc77ea257
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 16:44:08 +0530

    added more inputs and comments

--- Basics1/first.java
+++ Basics1/first.java
@@ -3,5 +3,7 @@ package Basics1;
 public class first { 
    public static void main(String[] args) {
         System.out.println("Namaste Ji, I am Ved"); // This will print the string
-   }
+        System.out.println(69); // This will print the numbers  
+        System.out.println(9+6);  // This will perform the operation and give the calculated output
+    }
 }
