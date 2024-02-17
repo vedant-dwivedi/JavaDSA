07b6b6c02266a562da99da5e7b61829f9582106b
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 02:14:43 +0530

    added: (assignment operator)

---
+++ Basics1/assignmentOperator.java
@@ -0,0 +1,27 @@
+package Basics1;
+
+public class assignmentOperator {
+    public static void main(String[] args) {
+        int x = 6;
+        x += 1;
+        System.out.println(x);
+        x -= 2;
+        System.out.println(x);
+        x *= 2;
+        System.out.println(x);
+        x /= 2;
+        System.out.println(x);
+        x %= 2;
+        System.out.println(x);
+        x &= 2; // Logical AND
+        System.out.println(x);
+        x |= 2; // Logical OR
+        System.out.println(x);
+        x ^= 2; // Bitwise exclusive OR
+        System.out.println(x);
+        x >>= 2; // Right Shift
+        System.out.println(x);
+        x <<= 2; // Left Shift
+        System.out.println(x);
+    }
+}
