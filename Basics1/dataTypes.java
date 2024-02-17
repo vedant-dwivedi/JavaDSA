c0c0d618accd57c61aedd22d09fbf4bc5d0dabf3
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 18:56:42 +0530

    added : (Data Types {primitive})

---
+++ Basics1/dataTypes.java
@@ -0,0 +1,15 @@
+package Basics1;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        int num = 69;
+        long numl = 6912369L;
+        double numd = 6.912369;
+        float numf = 6.9f;
+
+        System.out.println(num);
+        System.out.println(numl);
+        System.out.println(numd);
+        System.out.println(numf);
+    }
+}
