5b0cdff847c54fc2031063b835211e1b5771020e
vd0904 <vedantdwivedi094@gmail.com>
2024-02-17 18:11:27 +0530

    displaying variables

---
+++ displayVar.java
@@ -0,0 +1,7 @@
+public class displayVar { 
+   public static void main(String[] args) {
+    String name = "Vedant";
+    String surName = "Dwivedi";
+        System.out.println("Hi! myself " + name + " " + surName); 
+   }
+}
