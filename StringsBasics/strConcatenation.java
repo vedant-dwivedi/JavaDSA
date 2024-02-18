5f540b36555fc6b441bcd6b34fd5392196b90e07
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 17:41:49 +0530

    prop:(basic string props)

---
+++ StringsBasics/strConcatenation.java
@@ -0,0 +1,14 @@
+package StringsBasics;
+
+public class strConcatenation {
+    public static void main(String[] args) {
+        String str1 = "Vedant";
+        String str2 = "Dwivedi";
+        String str3 = "Web ";
+        String str4 = "Developer";
+        System.out.println(str1 + " " + str2);
+        System.out.println(str1 + " " + str2);
+        
+        System.out.println(str3.concat(str4));
+    }
+}
