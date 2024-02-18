c6dad91160e6056c74ca1ce5d82a98cb2c211e8a
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 17:56:52 +0530

    added: (basic maths property)

---
+++ MathsBasics/maths.java
@@ -0,0 +1,13 @@
+package MathsBasics;
+
+public class maths { 
+   public static void main(String[] args) {
+        System.out.println(Math.max(6, 9)); 
+        System.out.println(Math.min(6, 9)); 
+        System.out.println(Math.sqrt(6.9)); 
+        System.out.println(Math.abs(-6.9)); 
+        System.out.println(Math.random());
+        int ranNum = (int)(Math.random() *69); // it will print any random number between 0 to 68 
+        System.out.println(ranNum); 
+   }
+}
