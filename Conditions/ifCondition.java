588abc646e9a5d1244382f0a80bb33e3fcb4d439
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 19:01:51 +0530

    added: (if condition)

---
+++ Conditions/ifCondition.java
@@ -0,0 +1,10 @@
+package Conditions;
+
+public class ifCondition {
+   public static void main(String[] args) {
+      if (9 > 6) {
+
+         System.out.println("9 is greater than 6 ");
+      }
+   }
+}
