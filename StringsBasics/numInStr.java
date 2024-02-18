54924409f09abd22634e0e9ef927b8070363525b
vd0904 <vedantdwivedi094@gmail.com>
2024-02-18 17:48:11 +0530

    added: (numbers in strings )

---
+++ StringsBasics/numInStr.java
@@ -0,0 +1,11 @@
+package StringsBasics;
+
+public class numInStr {
+    public static void main(String[] args) {
+        String num1 = "10";
+        String num2 = "20";
+        int num3 = 30;
+        System.out.println(num1 + num2);
+        System.out.println(num1 + num2 + num3 );
+    }
+}
