d3998052a7764d7c2aabe9507eb2a419d03485dc
vd0904 <vedantdwivedi094@gmail.com>
2024-03-22 22:43:04 +0530

    done:forloop,2examples

---
+++ Loops/forLoop.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println(i + " This will print the nos till 9 ");
+        }
+
+        for(int i = 0; i < 69; i = i + 2){
+            System.out.println(i + " is the Even nos between 0 and 69");
+        }
+    }
+}
