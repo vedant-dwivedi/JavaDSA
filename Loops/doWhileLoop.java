e7eb433f7a58a20b79fcc93495ed34048d92760e
vd0904 <vedantdwivedi094@gmail.com>
2024-03-22 22:33:48 +0530

    edited

---
+++ Loops/doWhileLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+// import java.util.Scanner;
+
+public class doWhileLoop {
+   public static void main(String[] args) {
+      int i = 0;
+      do {
+         System.out.println(i + " Loop will run until number 9 ");
+         i++;
+      } while (i < 9);
+   }
+}
