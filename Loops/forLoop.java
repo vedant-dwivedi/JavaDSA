751adbaf96bed6dc76d91048f511bfe208958a38
vd0904 <vedantdwivedi094@gmail.com>
2024-03-22 22:50:58 +0530

    added: nested example

--- Loops/forLoop.java
+++ Loops/forLoop.java
@@ -4,10 +4,11 @@ public class forLoop {
     public static void main(String[] args) {
         for (int i = 0; i < 9; i++) {
             System.out.println(i + " This will print the nos till 9 ");
-        }
 
-        for(int i = 0; i < 69; i = i + 2){
-            System.out.println(i + " is the Even nos between 0 and 69");
+            // Nested Loop 
+            for (int j = 0; j < 69; j = j + 2) {
+                System.out.println(j + " is the Even nos between 0 and 69");  // phle ek i print hoga fir poora j run hoga, fir next i run hoga fir poora j ese nested work krega  
+            }
         }
     }
 }
