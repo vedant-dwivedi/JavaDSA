6e7dc2404a2092b3a3205bf950af99df3414e2d9
vd0904 <vedantdwivedi094@gmail.com>
2024-03-25 00:08:18 +0530

    done: for each Loop

---
+++ Loops/forEachLoop.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class forEachLoop {
+    public static void main(String[] args) {
+        String[] vowels = { "a", "e", "i", "o", "u" };
+        for (String i : vowels) {
+            System.out.println(i);
+        }
+    }
+}
