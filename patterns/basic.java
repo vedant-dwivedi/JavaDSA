6bd0a7b2abc045ced8b2520d5bef236bfa96aa71
vd0904 <vedantdwivedi094@gmail.com>
2024-06-02 22:50:24 +0530

    basic pattern (triangle)

---
+++ patterns/basic.java
@@ -0,0 +1,12 @@
+package patterns;
+
+public class basic {
+    public static void main(String[] args) {
+        for (int i = 1; i <=9; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
