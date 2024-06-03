ec2d4dd242d8eb0546b2bad84afedbd0f8648174
vd0904 <vedantdwivedi094@gmail.com>
2024-06-04 03:39:28 +0530

    square star pattern

--- patterns/SS.java
+++ patterns/SS.java
@@ -1,8 +1,14 @@
 package patterns;
+// Square Star pattern
 
 public class SS {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
-    
+
 }
