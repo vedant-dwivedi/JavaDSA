7bb6181cb3cb3d3955ffaf509a64f6230613438c
vd0904 <vedantdwivedi094@gmail.com>
2024-06-04 11:57:25 +0530

    Pyramid Star Pattern

---
+++ patterns/PS.java
@@ -0,0 +1,17 @@
+package patterns;
+
+// Pyramid Star Pattern
+
+public class PS {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
