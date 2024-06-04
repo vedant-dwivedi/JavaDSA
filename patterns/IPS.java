33ddbdc8d05789d7936a3a79fba1c44ff4a6a362
vd0904 <vedantdwivedi094@gmail.com>
2024-06-04 11:33:32 +0530

    IPS edited

---
+++ patterns/IPS.java
@@ -0,0 +1,17 @@
+package patterns;
+
+// Inverted Pyramid Pattern
+public class IPS {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
