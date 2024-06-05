73a04ae8009843ccc22d8ce1e5b0ffccb8794102
vd0904 <vedantdwivedi094@gmail.com>
2024-06-05 22:58:32 +0530

    edited

---
+++ ArraysBasics/array.java
@@ -0,0 +1,11 @@
+package ArraysBasics;
+
+// An array is a collection of similar types of data. 
+
+public class array {
+    public static void main(String[] args){
+        String[] pet = {"dog", "cat", "you"};
+        System.out.println(pet.length);
+    }
+    
+}
