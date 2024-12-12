635e9fc72d3eedc3cb4e602fc0df65596883d95b
vedant-dwivedi <vedantdwivedi094@gmail.com>
2024-12-12 23:35:42 +0530

    Edited

--- .history/ArraysBasics/array_20240605225750.java
+++ .history/ArraysBasics/array_20240605225750.java
@@ -1,6 +1,6 @@
 package ArraysBasics;
-// An array is a collection of similar types of data. 
 
+// An array is a collection of similar types of data. 
 
 public class array {
     public static void main(String[] args){
--- .history/ArraysBasics/array_20241212233438.java
+++ .history/ArraysBasics/array_20241212233438.java
@@ -1,7 +1,6 @@
 package ArraysBasics;
 // An array is a collection of similar types of data. 
 
-
 public class array {
     public static void main(String[] args){
         String[] pet = {"dog", "cat", "you"};
